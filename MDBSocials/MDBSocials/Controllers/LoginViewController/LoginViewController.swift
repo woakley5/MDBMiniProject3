be4588757a32e3edc310aa9acc0e3ab2c73888cb
Will Oakley <woakley5@icloud.com>
2018-02-19 22:34:22 -0800

    Initial commit

---
+++ MDBSocials/MDBSocials/Controllers/LoginViewController/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  MDBSocials
+//
+//  Created by Will Oakley on 2/19/18.
+//  Copyright © 2018 Will Oakley. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# MDBMiniProject3
